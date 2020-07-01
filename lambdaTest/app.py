2f1b878f8f6dd55b975565634a7e54bbbfaca011
Andrea Soriano <andreanicolasasoriano@gmail.com>
2020-07-01 15:13:57 +1000

    Access to s3, but there were access deny errors

--- lambdaTest/app.py
+++ lambdaTest/app.py
@@ -1,16 +1,31 @@
 from chalice import Chalice
+import boto3
 
 app = Chalice(app_name='lambdaTest')
+
+BUCKET = 'lawpath-data-lake'
+s3_resource=boto3.resource("s3")
 
 
 @app.lambda_function()
 def hello_world(event, context):
+    print("hello world!")
     return {'hello': 'world'}
 
 @app.lambda_function()
 def check_this_works(event, context):
-    return {"hello again"}
+    print("hello again!")
+    return {"hello": "again"}
 
+@app.lambda_function()
+def s3PrintFunction(event,context):
+    stuff = s3_resource.Bucket(BUCKET)
+    for obj in stuff.objects.all():
+        print(obj.key)
+
+#@app.on_s3_event(bucket=BUCKET, events=['s3:ObjectCreated:*'])
+#def prints3chalice(event,context):
+#    print(event)
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
