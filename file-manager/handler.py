f9c30a0e73577ad490b0a04fe7a55691e1a6ef5f
Andrea Soriano <andreanicolasasoriano@gmail.com>
2020-07-02 15:51:44 +1000

    changed function names, and added an sns handler, which triggers 'hello world' if the SNS topic's ARN contains 'lambda_topic'

--- file-manager/handler.py
+++ file-manager/handler.py
@@ -1,14 +1,16 @@
+from __future__ import print_function
 import json
 import boto3
 import gzip
 from io import BytesIO
 import inspect
 
+
 s3=boto3.resource('s3')
 bucket_name="lawpath-data-lake"
 key = "segment-logs/6Q2N6sz6Aq/1544572800000/1553116119647.b27b420ba21e.f4c8ef3.420dbd6d-9228-474b-9e11-1744e7626cb8.gz"
 
-def hello(event, context):
+def s3_handler(event, context):
     print("hello world!")
     json_list=[]
     json_return = []
@@ -33,17 +35,13 @@ def hello(event, context):
             print("Tracked Event: " + i["event"])
             json_return.append(i["event"])
             
-
-    
-    #print(json_list[0]["type"])
-    #print(type(json_list[0]))
-
-    
-    #content = gzipfile.read()
-    #json_data=[]
-    #for line in content:
-    #    json_line = json.loads(line)
-    #    json_data.append(json_line)
-    #print(json_data[0])
-    #print(type(json_data[0]))
     return json_return
+
+def sns_handler(event, context):
+    topicARN = event['Records'][0]['Sns']['TopicArn']
+    topic = "lambda_topic"
+    sns_return = ""
+    if topic in topicARN:
+        sns_return = event['Records'][0]['Sns']['TopicArn']
+        print("Hello world!")
+    return sns_return
--- hello-world/handler.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def hello(event, context):
-    print("hello world!")
-    return "hello-world"
